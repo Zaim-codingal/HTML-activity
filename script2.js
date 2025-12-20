c1464a0299f839c33f729564ad74eb7cc6e5c1f4
Zaim-codingal <okay.contentfr@gmail.com>
2025-12-20 12:45:47 +0500

    commit 1

---
+++ script2.js
@@ -0,0 +1,63 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const age = document.getElementById('age');
+const message = document.getElementById('message');
+
+email.addEventListener("input", validateEmail);
+password.addEventListener("input", validatePassword);
+age.addEventListener("input", validateAge);
+
+function validateEmail() {
+    if (email.value.includes('@') && email.value.includes('.')) {
+        setSuccess(email);
+        return true;
+    } else {
+        setError(email);
+        return false;
+    }
+}
+
+function validatePassword() {
+    if (password.value.length >= 8) {
+        setSuccess(password);
+        return true;
+    } else {
+        setError(password);
+        return false;
+    }
+}
+
+function validateAge() {
+    if (Number(age.value) >= 18) { 
+        setSuccess(age;
+        return true;
+    } else {
+        setError(age);
+        return false;
+    }
+}
+
+function setSuccess(input) {
+    input.classList.add("success");
+    input.classList.remove("error");
+}
+
+function setError(input) {
+    input.classList.add("error");
+    input.classList.remove("success");
+}
+
+function validate(event) {
+    event.preventDefault();
+    const isEmailValid = validateEmail();
+    const isPasswordValid = validatePassword();
+    const isAgeValid = validateAge();
+    
+    if (isEmailValid && isPasswordValid && isAgeValid){
+        message.style.color = "green";
+        message.innerHTML = "Login successful!";
+    } else {
+        message.style.color = "red";
+        message.innerHTML = "Unsuccessful login. Please correct the errors above.";
+    }
+}
